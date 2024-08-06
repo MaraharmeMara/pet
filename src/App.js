910e74a03a84d76d87026d2c12caec34a2ed7355
MaraharmeMara <kotova.mariia94@gmail.com>
2024-08-06 10:46:08 +0300

    top and bottom menu added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
+import { createRoot } from "react-dom/client";
+
 function App() {
   return (
     <div className="App">
@@ -13,5 +15,8 @@ function App() {
     </div>
   );
 }
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,40 @@
 import React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import FolderIcon from "@mui/icons-material/Folder";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 function Footer() {
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="footer">
-      <div>
-        <h4>Pets App</h4>
-      </div>
-    </div>
+    <BottomNavigation value={value} onChange={handleChange} className="footer">
+      <BottomNavigationAction
+        label="Recents"
+        value="recents"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="nearby"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction
+        label="Folder"
+        value="folder"
+        icon={<FolderIcon />}
+      />
+    </BottomNavigation>
   );
 }
 export default Footer;
