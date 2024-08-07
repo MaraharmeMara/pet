bf95397633f077976b12ace28d6662ea1616b8cd
MaraharmeMara <kotova.mariia94@gmail.com>
2024-08-07 12:50:59 +0300

    map works and marks paws added

--- src/components/CustomMap.js
+++ src/components/CustomMap.js
@@ -1,12 +1,58 @@
 import React, { useState } from "react";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import PetsIcon from "@mui/icons-material/Pets";
 
 const CustomMap = () => {
-  // shows marker on London by default
-  const [markerLocation, setMarkerLocation] = useState({
-    lat: 51.509865,
-    lng: -0.118092,
+  // shows Joensuu by default
+  const [markerLocation] = useState({
+    lat: 62.60070051849284,
+    lng: 29.765676378954662,
   });
+
+  const Pin = PetsIcon;
+
+  type Poi = { key: string, location: google.maps.LatLng };
+  const location: Poi[] = [
+    {
+      key: "Avaarannan koirapuisto",
+      location: { lat: 62.6139119100737, lng: 29.694705008595786 },
+    },
+
+    {
+      key: "Koivuniemen koirapuisto",
+      location: { lat: 62.59454388151994, lng: 29.745074788224024 },
+    },
+
+    {
+      key: "Niinivaaran koirapuisto",
+      location: { lat: 62.59470190717925, lng: 29.783526935009917 },
+    },
+
+    {
+      key: "Koirametsä Vekkula",
+      location: { lat: 62.66519627875552, lng: 29.512561231940342 },
+    },
+  ];
+  const PoiMarkers = (props: { pois: Poi[] }) => {
+    return (
+      <>
+        {props.pois.map((poi: Poi) => (
+          <AdvancedMarker key={poi.key} position={poi.location}>
+            <Pin
+              background={"#FBBC04"}
+              glyphColor={"#000"}
+              borderColor={"#000"}
+            />
+          </AdvancedMarker>
+        ))}
+      </>
+    );
+  };
 
   return (
     <div className="map-container">
@@ -14,11 +60,12 @@ const CustomMap = () => {
         <Map
           style={{ borderRadius: "20px" }}
           defaultZoom={13}
-          defaultCenter={markerLocation}
+          defaultCenter={{ lat: 62.60070051849284, lng: 29.765676378954662 }}
+          mapId="map"
           gestureHandling={"greedy"}
           disableDefaultUI
         >
-          <Marker position={markerLocation} />
+          <PoiMarkers pois={location} />
         </Map>
       </APIProvider>
     </div>
